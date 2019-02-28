dcce0aa71de5e9d045537fe280593317fb65ee3a
rendrap <rendrapradana@gmail.com>
2019-02-28 22:33:41 +0700

    add Redirect to Courses

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,17 +1,29 @@
 import React from 'react';
+import {
+  Route,
+  NavLink,
+  Redirect
+} from 'react-router-dom';
+import HTML from './courses/HTML';
+import CSS from './courses/CSS';
+import JavaScript from './courses/JavaScript';
 
 const Courses = () => (
-  <div className="main-content courses">
-    <div className="course-header group">
-      <h2>Courses</h2> 
-      <ul className="course-nav">
-        <li><a href='/courses/html'>HTML</a></li>
-        <li><a href='/courses/css'>CSS</a></li>
-        <li><a href='/courses/javascript'>JavaScript</a></li>
+  <div className='main-content courses'>
+    <div className='course-header group'>
+      <h2>Courses</h2>
+      <ul className='course-nav'>
+        <li><NavLink to='/courses/html'>HTML</NavLink></li>
+        <li><NavLink to='/courses/css'>CSS</NavLink></li>
+        <li><NavLink to='/courses/javascript'>JavaScript</NavLink></li>
       </ul>
     </div>
-    
-    {/* Write routes here... */}
+
+    {/* The Routes */}
+    <Redirect to='/courses/html' />
+    <Route path='/courses/html' component={HTML} />
+    <Route path='/courses/css' component={CSS} />
+    <Route path='/courses/javascript' component={JavaScript} />
   </div>
 );
 
